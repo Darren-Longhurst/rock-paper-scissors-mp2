bf85a668b63d81479fb0abe4ab5be4888fa748e2
Darren-Longhurst <dmlonghurst@icloud.com>
2025-09-10 20:46:32 +0100

    Determine winner function and check game function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,36 @@ $(document).ready(function () {
       return "scissors";
     }
   }
+
+    // Determine winner
+  function getWinner(player, computer) {
+    if (player === computer) {
+      return "tie";
+    } else if (
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissors" && computer === "paper")
+    ) {
+      return "player";
+    } else {
+      return "computer";
+    }
+  }
+
+  // Check if game is over
+  function checkGameOver() {
+    if (playerScore >= winScore || computerScore >= winScore) {
+      let finalMessage = "";
+      if (playerScore > computerScore) {
+        finalMessage = "Great job, you've won the game!";
+        $("#result").html(`<h2>${finalMessage}</h2>`);
+        $("#result").removeClass("game-win game-draw game-lose").addClass("game-win");
+      } else {
+        finalMessage = "Damn it, computer won the game!";
+        $("#result").html(`<h2>${finalMessage}</h2>`);
+        $("#result").removeClass("game-win game-draw game-lose").addClass("game-lose");
+      }
+
+      $(".choice").css("pointer-events", "none");
+    }
+  }
