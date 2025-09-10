80b206f26f3e6b104dfaecaa3294cbeb1d9fa496
Darren-Longhurst <dmlonghurst@icloud.com>
2025-09-10 20:38:51 +0100

    Global scope JS variables added and FA icons mapped to choice JS

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+ //Global variables
+$(document).ready(function () {
+  let playerScore = 0;
+  let computerScore = 0;
+  let tieScore = 0;
+  const winScore = 5;
+
+    // Map choice to Font Awesome icon
+  function getChoiceIcon(choice) {
+    if (choice === "rock") {
+      return '<i class="fas fa-hand-rock"></i>';
+    } else if (choice === "paper") {
+      return '<i class="fas fa-hand-paper"></i>';
+    } else {
+      return '<i class="fas fa-hand-scissors"></i>';
+    }
+  }
+})
